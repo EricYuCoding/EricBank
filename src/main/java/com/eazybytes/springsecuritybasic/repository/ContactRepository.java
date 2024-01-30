e2157511287eeb7426aa1d10a7ee758d08d7d578
melbourneeric <yaoyu201@gmail.com>
2024-01-30 16:17:31 +1030

    Please enter the commit message for your changes. Lines starting

--- src/main/java/com/eazybytes/springsecuritybasic/repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eazybytes.springsecuritybasic.repository;
-
-import com.eazybytes.springsecuritybasic.model.Contact;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends CrudRepository<Contact, Long> {
-}
